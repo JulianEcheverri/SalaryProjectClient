3683ec14bcc384a8083cade7a29ce49053e69fb1
JulianEcheverri <julianecheverri@outlook.com>
2020-07-15 17:37:29 -0500

    Adding sweetalert2 and showing messages to the user

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
 import { EmployeeModel } from '../../models/employee-model';
 import { EmployeeServiceService } from 'src/app/services/employee-service.service';
+import Swal from "sweetalert2";
 
 @Component({
   selector: 'app-home',
@@ -20,10 +20,20 @@ export class HomeComponent implements OnInit {
   }
 
   getEmployee() {
+    Swal.fire({
+      title: "Searching",
+      text: "Searching employees",
+      icon: "info",
+      allowOutsideClick: false
+    });
+
+    Swal.showLoading();
+    
     if (this.id === null || this.id === '') {
       this.employeeServiceService.getEmployees().subscribe((resp: any) => {
         console.log(resp);
         this.employeeList = resp;
+        Swal.close();
       });
     }
     else {
@@ -31,12 +41,26 @@ export class HomeComponent implements OnInit {
         this.employee = resp;
         this.employeeList = [];
         this.employeeList.push(this.employee);
+        Swal.close();
         // console.log(this.employee);
       }, (error: any) => {
+        Swal.fire({
+          title: '',
+          text: "Can't find the employee",
+          icon: "error",
+        });
         console.log(error);
         this.employeeList = [];
       });
+    }    
+  }
+
+  numberOnly(event: any): boolean {
+    const charCode = (event.which) ? event.which : event.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+      return false;
     }
+    return true;
   }
 
 }
